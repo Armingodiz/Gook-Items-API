15f36d9cfd35cee02bf95ede52f7892a01fc9a60
ArminGodiz <armingodarzi1380@gmail.com>
2021-02-19 16:00:39 +0330

    ItemService interface added

--- services/items_service.go
+++ services/items_service.go
@@ -1,1 +1,25 @@
 package services
+
+import (
+	"github.com/ArminGodiz/Gook-Items-API/domain/items"
+	"github.com/ArminGodiz/Gook-Items-API/utils/rest_errors"
+)
+
+var (
+	ItemService itemServiceInterface = &itemsService{}
+)
+
+type itemServiceInterface interface {
+	Create(items.Item) (*items.Item, *rest_errors.RestErr)
+	Get(string) (*items.Item, *rest_errors.RestErr)
+}
+type itemsService struct {
+}
+
+func (s *itemsService) Create(item items.Item) (*items.Item, *rest_errors.RestErr) {
+	return nil, rest_errors.NewInternalServerError("implementing ...")
+}
+
+func (s *itemsService) Get(id string) (*items.Item, *rest_errors.RestErr) {
+	return nil, rest_errors.NewInternalServerError("implementing ...")
+}
