c9743a5be4555c070e82838ddbb2145c7f1d56b1
ArminGodiz <armingodarzi1380@gmail.com>
2021-02-19 16:09:45 +0330

    Create handler added to controller

--- controller/items_controller.go
+++ controller/items_controller.go
@@ -1,1 +1,27 @@
 package controller
+
+import (
+	"fmt"
+	"github.com/ArminGodiz/Gook-Items-API/domain/items"
+	"github.com/ArminGodiz/Gook-Items-API/services"
+	"github.com/ArminGodiz/Gook-Items-API/services/oauth"
+	"net/http"
+)
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	if err := oauth.AuthenticateRequest(r); err != nil {
+		// Todo : return err to caller
+	}
+	item := items.Item{
+		Seller: oauth.GetCallerId(r),
+	}
+	result, err := services.ItemService.Create(item)
+	if err != nil {
+		// TODO : return err to caller
+	}
+	fmt.Println(result)
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+
+}
