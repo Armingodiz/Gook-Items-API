3ecc906693d1d06601e1d5bab91bfd600e70f693
ArminGodiz <armingodarzi1380@gmail.com>
2021-02-19 14:55:06 +0330

    Item entity defined

--- domain/items/item.go
+++ domain/items/item.go
@@ -1,1 +1,24 @@
 package items
+
+type Item struct {
+	Id string `json:"id"`
+	Seller int64 `json:"seller"`
+	Title string `json:"title"`
+	Description Description `json:"description"`
+	Pictures []Picture `json:"pictures"`
+	Video string `json:"video"`
+	Price float32 `json:"price"`
+	AvailableQuantity int `json:"available_quantity"`
+	Sold int `json:"sold"`
+	Status string `json:"status"`
+}
+
+type Description struct {
+	PlainText string `json:"plain_text"`
+	Html string `json:"html"`
+}
+
+type Picture struct {
+	Id int64 `json:"id"`
+	Url string `json:"url"`
+}
